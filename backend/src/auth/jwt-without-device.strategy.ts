b7a5243397a1fe0fac2ff0de87e6b116221195c8
Karel Dohnal <karel.dohnal@gmail.com>
2023-12-30 12:40:37 +0000

    Admin access + url persistency

--- backend/src/auth/jwt-without-device.strategy.ts
+++ backend/src/auth/jwt-without-device.strategy.ts
@@ -1,13 +1,12 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import * as dotenv from "dotenv";
-import {AuthService} from "./auth.service";
 dotenv.config();
 
 @Injectable()
 export class JwtWithoutDeviceStrategy extends PassportStrategy(Strategy, "jwt-without-device") {
-  constructor(private authService: AuthService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
