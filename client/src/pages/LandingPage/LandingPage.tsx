f54c979341eeb00dc1ed832f23dfec7f5799c7f0
kareldohnal <karel.dohnal@gmail.com>
2024-06-02 20:32:30 +0200

    Landing page - hero section

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,16 +1,40 @@
-import LandingPageHeader from "../../components/LandingPageHeader/LandingPageHeader";
 import LayoutPublicWrapper from '../../wrappers/LayoutPublicWrapper';
+import { FLAG_PRESALE } from '../../dev_flags';
+import { FormattedMessage } from 'react-intl';
+import ButtonColored from '../../components/Button/ButtonColored';
+import "./landingPage.scss"
 
-type Props = {
+const LandingPage = () => {
+    return (
+        <LayoutPublicWrapper>
+            <div className={'landingPage'}>
 
-};
+                <section className={'landingPage__hero'}>
+                    <h1>Civic Tech Market Report 2025</h1>
+                    <p>{FLAG_PRESALE
+                        ? <FormattedMessage
+                            id={'text_hero_presale'}
+                            defaultMessage={'Explore major trends that are currently shaping the field of digital participation. Get access to the insights from top-tear industry experts and gain a perfect overview of the European Civic Tech ecosystem and detailed information about over 80 solutions with accompanying case studies. Sign up now to pre-order your copy of Civic Tech Market Report 2025 for the early-bird price!'}
+                        />
+                        : <FormattedMessage
+                            id={'text_hero'}
+                            defaultMessage={'Explore major trends that are currently shaping the field of digital participation. Get access to the insights from top-tear industry experts and gain a perfect overview of the European Civic Tech ecosystem and detailed information about over 80 solutions with accompanying case studies.'}
+                        />
+                    }</p>
+                    <ButtonColored size={'large'}>
+                        <a href={'#pricingSection'}>
+                            {FLAG_PRESALE
+                                ? <FormattedMessage id={'label_preorder'} defaultMessage={'Pre-order'} />
+                                : <FormattedMessage id={'label_get_your_copy'} defaultMessage={'Get your copy'} />
+                            }
 
-const LandingPage = ({}: Props) => {
- return (
-  <LayoutPublicWrapper>
-        Landing Page
-  </LayoutPublicWrapper>
- );
+                        </a>
+                    </ButtonColored>
+                </section>
+
+            </div>
+        </LayoutPublicWrapper>
+    );
 };
 
 export default LandingPage;
