119ac2681429180a30ae6ca98254d4e67d7607f3
kareldohnal <karel.dohnal@gmail.com>
2024-07-30 17:56:41 +0200

    Checkout

--- client/src/components/CheckoutForm/CheckoutForm.tsx
+++ client/src/components/CheckoutForm/CheckoutForm.tsx
@@ -56,8 +56,8 @@ export default function CheckoutForm() {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: 'http://localhost:5173/order/success',
-                // return_url: 'https://civictechreport.com/order/success',
+                // return_url: 'http://localhost:5173/order/success',
+                return_url: 'https://civictechreport.com/order/success',
                 shipping: {
                     name: 'Krasomil Mladistfvý',
                     address: {
