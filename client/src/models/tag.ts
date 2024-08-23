de94fb194038c202508db0499c3b03d3ddae74bb
kareldohnal <karel.dohnal@gmail.com>
2024-08-23 18:35:41 +0200

    Tool Detail

--- client/src/models/tag.ts
+++ client/src/models/tag.ts
@@ -27,3 +27,25 @@ export const socialTagsIntl = [
     { label: 'label_stag_democratic_governance_and_innovation', value: 'democratic_governance_and_innovation', defaultMessage: 'Democratic Governance and Inovation' },
     { label: 'label_stag_sustainable_urban_development', value: 'sustainable_urban_development', defaultMessage: 'Sustainable Urban Development' },
 ];
+
+export const featureTagIntl = [
+    { label: "label_ftag_online_mass_communication", value: "online_mass_communication", defaultMessage: "Online Mass Communication" },
+    { label: "label_ftag_info_hub", value: "info_hub", defaultMessage: "Info Hub" },
+    { label: "label_ftag_voting", value: "voting", defaultMessage: "Voting" },
+    { label: "label_ftag_survey", value: "survey", defaultMessage: "Survey" },
+    { label: "label_ftag_idea_gathering", value: "idea_gathering", defaultMessage: "Idea Gathering" },
+    { label: "label_ftag_cocreation", value: "cocreation", defaultMessage: "Co-creation" },
+    { label: "label_ftag_dialogue", value: "dialogue", defaultMessage: "Dialogue" },
+    { label: "label_ftag_moderation", value: "moderation", defaultMessage: "Moderation" },
+    { label: "label_ftag_multichannel_communication", value: "multichannel_communication", defaultMessage: "Multichannel Communication" },
+    { label: "label_ftag_data_analysis", value: "data_analysis", defaultMessage: "Data Analysis" },
+    { label: "label_ftag_hardware", value: "hardware", defaultMessage: "Hardware" },
+    { label: "label_ftag_future_tech", value: "future_tech", defaultMessage: "Future Tech" },
+    { label: "label_ftag_data_mapping", value: "data_mapping", defaultMessage: "Data Mapping" },
+    { label: "label_ftag_contact_management", value: "contact_management", defaultMessage: "Contact Management" },
+    { label: "label_ftag_petition", value: "petition", defaultMessage: "Petition" },
+    { label: "label_ftag_crowdfunding", value: "crowdfunding", defaultMessage: "Crowdfunding" },
+    { label: "label_ftag_local_currency", value: "local_currency", defaultMessage: "Local Currency" }
+];
+
+
--- client/src/pages/ToolDetail/ToolDetail.tsx
+++ client/src/pages/ToolDetail/ToolDetail.tsx
@@ -3,13 +3,16 @@ import { useEffect, useState } from 'react';
 import { getToolsById } from '../../api/tools';
 import { ToolEntity } from '../../models/tools';
 import Header from '../../components/Header/Header';
+import { accessibilityTagsIntl, featureTagIntl, socialTagsIntl } from '../../models/tag';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 const ToolDetail = () => {
     const [tool, setTool] = useState<ToolEntity>({} as ToolEntity);
     const { id } = useParams();
+    const intl = useIntl();
 
     useEffect(() => {
-        if (!id) return
+        if (!id) return;
         getToolsById(id)
             .then((response) => {
                 console.log(response);
@@ -23,13 +26,93 @@ const ToolDetail = () => {
     return (
         <div>
             <Header />
-            <h1>Tool Detail</h1>
             <div>
-                <h2>{tool.toolsName}</h2>
-                <p>{tool.descEn}</p>
+                <div>
+                    <div>
+                        <h1>{tool.toolsName}</h1>
+                        <p>{tool.descEn}</p>
+                    </div>
+                    <img src={tool.logo} alt={"logo"}/>
+                </div>
+                <div>
+                    {tool.featureTag?.map((feature, index) => {
+                        const intlObject = featureTagIntl.find((tag) => tag.value === feature);
+                        return (
+                            <div key={index}>
+                                <FormattedMessage id={intlObject?.label} defaultMessage={intlObject?.defaultMessage} />
+                            </div>
+                        );
+                    })}
+                </div>
+                <div>
+                    <h2>Standout features</h2>
+                    <ul>
+                        {tool.featuresEn?.map((feature, index) => {
+                            return <li key={index}>{feature}</li>;
+                        })}
+                    </ul>
+                </div>
+                <div>
+                    <h2>Tool Details</h2>
+                    <div>
+                        <div>
+                            <h4>Data Protection</h4>
+                            <p>{tool.dataProtectionEn}</p>
+
+                            <h4>Authentication/Verification</h4>
+                            <p>{tool.authenticationEn}</p>
+
+                            <h4>Year established</h4>
+                            <p>{tool.established}</p>
+
+                            <h4>Number of clients</h4>
+                            <p>{tool.noOfClients}</p>
+                        </div>
+                        <div>
+                            <h4>Team</h4>
+                            <p>{tool.team}</p>
+
+                            <h4>Countries active</h4>
+                            <p>{tool.countries?.join(', ').toUpperCase()}</p>
+
+                            <h4>Next product update session</h4>
+                            <p>{tool.nextProductUpdateEn}</p>
+
+                            <h4>Network and partners</h4>
+                            <p>{tool.partners}</p>
+                        </div>
+                        <div>
+                            <div>
+                                <h4>Accessibility</h4>
+                                {tool.accessibilityTag?.map((atag, index) => {
+                                    const intlObject = accessibilityTagsIntl.find((tag) => tag.value === atag);
+                                    return (
+                                        <div key={index}>
+                                            <FormattedMessage id={intlObject?.label} defaultMessage={intlObject?.defaultMessage} />
+                                        </div>
+                                    );
+                                })}
+
+                                <h4>Social positioning</h4>
+                                {tool.socialPositioningTag?.map((stag, index) => {
+                                    const intlObject = socialTagsIntl.find((tag) => tag.value === stag);
+                                    return (
+                                        <div key={index}>
+                                            <FormattedMessage id={intlObject?.label} defaultMessage={intlObject?.defaultMessage} />
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            <h4>Contact</h4>
+                            <p>{tool.email}</p>
+                            <p>{tool.phone}</p>
+                            <p>{tool.web}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default ToolDetail;
