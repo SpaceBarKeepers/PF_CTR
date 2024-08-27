7c989c540a2cdc6e498550e67486533eb8d15e1a
kareldohnal <karel.dohnal@gmail.com>
2024-08-27 18:49:29 +0200

    Hamburger menu

--- client/src/components/Button/ButtonColored.tsx
+++ client/src/components/Button/ButtonColored.tsx
@@ -4,6 +4,7 @@ import "./buttons.scss"
 type Props = {
     children: React.ReactNode,
     childIsLink?: boolean,
+    className?: string,
     disabled?: boolean,
     id?: string,
     onClick?: () => void,
@@ -13,10 +14,10 @@ type Props = {
     type?: "button" | "submit" | "reset"
 }
 
-const ButtonColored = ({ children, childIsLink = true, disabled = false, id, onClick, size = "normal", style, buttonType = "primary", type }: Props) => {
+const ButtonColored = ({ children, childIsLink = true, disabled = false, className, id, onClick, size = "normal", style, buttonType = "primary", type }: Props) => {
     return (
         <button
-            className={`button${buttonType === "secondary" ? " buttonSecondary" : " buttonPrimary"}${size === "large" ? " buttonLarge" : ""}${childIsLink ? "" : " buttonWithoutLink"}`}
+            className={`button${buttonType === "secondary" ? " buttonSecondary" : " buttonPrimary"}${size === "large" ? " buttonLarge" : ""}${childIsLink ? "" : " buttonWithoutLink"}${className ? ` ${className}` : ""}`}
             {...{disabled, id, onClick, style, type}}
         >
             {children}
